957fead42773bace028b3fd92da2d65e8102e62c
Malek Salem <malek.salem.14@gmail.com>
2020-02-05 20:40:13 +0000

    add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - main function uses printf func
+  * Return: 0
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

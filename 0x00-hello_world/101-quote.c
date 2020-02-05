d712a337ce98ae3e6ca9fe0b8511a0fb74bbd37e
Malek Salem <malek.salem.14@gmail.com>
2020-02-05 21:51:07 +0000

    fix code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
   * main - entry point function
   * Return: 1
@@ -8,6 +9,6 @@ int main(void)
 {
 	char str[] =
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fprintf(stderr, str, sizeof(str));
+	write(2, str, sizeof(str));
 	return (1);
 }

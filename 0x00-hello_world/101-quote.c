094d0fcf5d5be6d1cdc82996af67470b7d385781
Malek Salem <malek.salem.14@gmail.com>
2020-02-05 21:59:12 +0000

    fix code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 	char str[] =
-		"and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n";
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	write(2, str, sizeof(str));
 	return (1);
 }

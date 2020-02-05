99eb1ee040b239dedad294a5050a197d5b81874d
Malek Salem <malek.salem.14@gmail.com>
2020-02-05 20:18:20 +0000

    update code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - main Function uses puts
  * Return: 0 
 */
 
 int main(void)
-
 {
+
 	puts("\"Programming is like building a multilingual puzzlei");
 	return(0);
+
 }

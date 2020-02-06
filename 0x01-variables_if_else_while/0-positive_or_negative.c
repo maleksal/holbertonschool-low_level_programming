fb65f5bd7470952da4d4eb137cde972789af482b
Malek Salem <malek.salem.14@gmail.com>
2020-02-06 12:55:29 +0000

    update code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,11 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive\n",n);
-	}else{
+	}
+	else if ( n == 0)
+	{
+		printf("%d is zero\n",n)
+	else{
 		printf("%d is negative\n",n);
 		return (0);
 	}

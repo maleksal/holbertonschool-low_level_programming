84a478cc27242d0b08c13ae2f0d71a18aee58966
Malek Salem <malek.salem.14@gmail.com>
2020-02-06 13:16:09 +0000

    update code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,8 +23,8 @@ int main(void)
 	}
 	else if (lastdigit < 6)
 	{
-		printf("last digit of %d is %d
-			and is less than 6 and not 0\n", n, lastdigit);
+		printf("last digit of %d is %d and\
+				is less than 6 and not 0\n", n, lastdigit);
 	}
 	else if (lastdigit == 0)
 	{

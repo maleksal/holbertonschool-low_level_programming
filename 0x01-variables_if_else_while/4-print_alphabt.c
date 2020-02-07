21f535bb928ba36cdecfddd2a766ac5adc1a5644
Malek Salem <malek.salem.14@gmail.com>
2020-02-07 17:45:00 +0000

    add task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - entry function
+  * Return: 0
+  */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' || c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

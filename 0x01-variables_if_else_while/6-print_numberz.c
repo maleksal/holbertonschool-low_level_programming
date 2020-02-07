757f83f72cf19c56c1038788ce2441e315780168
Malek Salem <malek.salem.14@gmail.com>
2020-02-07 19:11:57 +0000

    add task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - entry function
+  * Return: 0
+  */
+
+int main(void)
+{
+	/* Code goes here */
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}

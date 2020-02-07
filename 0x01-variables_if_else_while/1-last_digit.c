ca7909e6198664fd66969fc3105e5faa001ad915
Malek Salem <malek.salem.14@gmail.com>
2020-02-07 16:21:35 +0000

    fix line length

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,14 +16,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
 	if (lastdigit > 5)
-	{
+
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
-	} else if (lastdigit == 0)
-	{
+	else if (lastdigit == 0)
+
 		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
-	} else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
-	}
+	else
+
+		printf(
+			"Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 	return (0);
 }

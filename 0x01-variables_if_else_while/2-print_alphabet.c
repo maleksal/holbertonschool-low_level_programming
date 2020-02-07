169d659cfd56467c5bf6ee67cbe0d21202b617c6
Malek Salem <malek.salem.14@gmail.com>
2020-02-07 16:37:54 +0000

    add task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - entry function
+  * Return: 0
+  */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

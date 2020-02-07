53a35256e75b1fb78ba327a6fd16f99ed04ca5ca
Malek Salem <malek.salem.14@gmail.com>
2020-02-07 19:17:06 +0000

    add task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - entry function
+  * Return: 0
+  */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

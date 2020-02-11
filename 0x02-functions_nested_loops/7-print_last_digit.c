3d9f3849efc0573b96d54f8965c1b5c952030dcf
Malek Salem <malek.salem.14@gmail.com>
2020-02-11 10:36:06 +0000

    add task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  *  print_last_digit - return last digit of a number
+  *  ------------------
+  *
+  *  @digit: takes an int
+  *
+  *  Return: last digit of number
+  *
+ **/
+
+
+int print_last_digit(int digit)
+{
+	return (digit % 10);
+}

2cdb71842aac1f7ae995ca0c3e6c6fc3430b48f5
Malek Salem <malek.salem.14@gmail.com>
2020-02-11 20:17:43 +0000

    add task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  *  add - adds two numbers
+  *  ------------------
+  *
+  *  @a: takes an int
+  *  @b: takes an int
+  *
+  *  Return: return a + b
+  *
+ **/
+
+int add(int a, int b)
+{
+	return (a + b);
+}

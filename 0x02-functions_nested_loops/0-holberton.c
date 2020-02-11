4ddd36b703b7cb92dbca4081640fcb9c81e5ad00
Malek Salem <malek.salem.14@gmail.com>
2020-02-11 08:28:51 +0000

    update code

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -12,14 +12,27 @@
 
 int main(void)
 {
-	char char_array[] = "Holberton";
-	int index, array_size;
+	/*
+	 * -- code fails --
+	 *
+	 * char char_array[] = "Holberton";
+	 * int index, array_size;
 
-	array_size = sizeof(char_array) / sizeof(char);
-	for (index = 0; index != array_size; index++)
-	{
-		_putchar(char_array[index]);
-	}
-	_putchar('\n');
-	return (0);
+	 * array_size = sizeof(char_array) / sizeof(char);
+	 * for (index = 0; index != array_size; index++)
+	 * {
+	 *	_putchar(char_array[index]);
+	 * }
+	 * _putchar('\n');
+	 * return (0);
+	*/
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 }

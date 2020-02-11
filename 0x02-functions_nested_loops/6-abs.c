f45fff857a58b489dd5b26f4bc2779a0cba7e7ac
Malek Salem <malek.salem.14@gmail.com>
2020-02-11 10:20:53 +0000

    add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  *  _abs - computes absolute value of an int
+  *  ------------------
+  *
+  *  @n: takes an int
+  *
+  *  Return: return absolute value
+  *
+ **/
+
+int _abs(int n)
+{
+	return (n *= -1);
+}

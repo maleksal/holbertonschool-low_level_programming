f37c2e21b74bceb0bac4659a1775b7d4a0b57dce
Malek Salem <malek.salem.14@gmail.com>
2020-02-11 09:38:35 +0000

    add task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  * _isalpha - checks for upper case char
+  *  ------------------
+  *
+  *  @c: takes an int ( ASCII code characters)
+  *
+  *  Return: return 1 if uppercase OR 0 if not
+  *
+ **/
+
+int _isalpha(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	} return (0);
+}

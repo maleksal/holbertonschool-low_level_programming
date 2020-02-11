9a0fb7e313caa336015ed0950b176134a7aedc29
Malek Salem <malek.salem.14@gmail.com>
2020-02-11 09:55:25 +0000

    add task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+/**
+  * print_sign - prints the sign of a number
+  *		positive, negative, zero
+  * -------------------------
+  *
+  *  @n: takes an int
+  *
+  *  Return: return 1 OR 0 OR -1
+  *
+ **/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+
+}

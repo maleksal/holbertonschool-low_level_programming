f16f68828470dcc77c39c20a5742b04383204764
Malek Salem <malek.salem.14@gmail.com>
2020-02-12 20:05:35 +0000

    add task 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * main - print sum of multiple 3 && 5
+  *  ------------------
+  *
+  *  @void: no param
+  *
+  *  Return: 0
+  *
+ **/
+
+int main(void)
+{
+	int num;
+	int sum;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if (num % 5 == 0 || num % 3 == 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	retrun (0);
+}
+

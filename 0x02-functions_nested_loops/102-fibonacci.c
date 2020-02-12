48462f6c8e54fe6ae0f0722a495809f27a20c71b
Malek Salem <malek.salem.14@gmail.com>
2020-02-12 20:56:54 +0000

    update code

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,23 +12,18 @@
 
 int main(void)
 {
-	int num1 = 1;
-	int num2 = 2;
+	long int num1 = 1;
+	long int num2 = 2;
 	int i;
-	int fib;
+	long int fib;
 
-	for (i = 1; i <= 50; i++)
+	printf("%lu, %lu, ", num1, num2);
+	for (i = 1+2; i <= 50; i++)
 	{
-		if (i <= 2)
-		{
-			fib = i;
-		} else
-		{
-			fib = num1 + num2;
-			num1 = num2;
-			num2 = fib;
-		}
-		printf("%d", fib);
+		fib = num1 + num2;
+		num1 = num2;
+		num2 = fib;
+		printf("%lu", fib);
 		if (i < 50)
 		{
 			printf(", ");

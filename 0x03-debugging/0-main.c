5fad173dc7b5efe12fc337cec7d4be37c129f3cf
Malek Salem <malek.salem.14@gmail.com>
2020-02-12 21:15:50 +0000

    fix code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
-#include "holberon.h"
+#include "holberton.h"
 
 /**
-  * main - tests function that prints if integer is positive or negative
-  * Return: 0
-  */
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {

7e1384fd0010e8d6266072c9ae3aa8df1535ab16
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 19:55:31 +0000

    add task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+  * print_triangle - print triangle with #
+  *
+  * @size: takes an int
+  * Return: implicit return
+  */
+
+void print_triangle(int size)
+{
+	int i;
+	int a = 1;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	while (a < size)
+	{
+		for (i = 0; i <= size - a; i++)
+		{
+			_putchar(' ');
+		}
+		for (b = 0; b < a; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		a++;
+	}
+}

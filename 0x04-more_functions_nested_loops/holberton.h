dadbf0050f456da359c3c6337836d73de7bd3d70
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 14:20:44 +0000

    update

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,7 +2,6 @@ int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void numbers(void);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);

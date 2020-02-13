6b78f36b24d4b125a045dbcc47e21d1f3a42ce28
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 08:39:38 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  *  _isupper - check for uppercase letters
+  *  ------------------
+  *
+  *  @c: takes an int (ASCII code characters)
+  *
+  *  Return: 0 OR 1
+  *
+ **/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+	{
+		return (1);
+	} return (0);
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,6 @@
+#ifdef HOLBERTON
+#define HOLBERTON
+
+int _putchar(int c);
+int _isupper(int c);
+#endif

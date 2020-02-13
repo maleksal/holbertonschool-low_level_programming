c82af4eba32fc87bcfd2977cf85887fa9f6aa1b2
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 09:36:57 +0000

    add func mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h" 
+
+/**
+  *  mul - multiplies two ints
+  *  ------------------------
+  *
+  *  @a: takes an int
+  *  @b: takes an int
+  *  Return: a + b
+ **/
+
+int mul(int a, int b);
+{
+	return (a + b);
+}
+
+

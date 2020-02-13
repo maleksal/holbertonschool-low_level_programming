2d9796448d7c7bdba869095612f1cf3d90593a9b
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 10:30:26 +0000

    update file

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,13 +1,10 @@
-#ifdef HOLBERTON
-#define HOLBERTON
-
-int _putchar(int c);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
-#endif
 

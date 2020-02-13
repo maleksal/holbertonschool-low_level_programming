ab044f57fcf08de29c90b780163a622e9110280d
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 18:34:27 +0000

    update code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,21 @@
  **/
 
 
-int main()
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= (100 + 1); i++)
 	{
-		if ( i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz\n");
 		}
-		else if ( i % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz\n");
 		}
-		else if ( i % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz\n");
 		}

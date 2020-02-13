7cf1c076f064fff80e82fa93d98120fdd36757f1
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 12:34:18 +0000

    fix bug

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -16,10 +16,11 @@ void print_most_numbers(void)
 
 	while (i <= 9)
 	{
-		if (i != 2 || i != 4)
+		if (i == 2 || i == 4)
 		{
-			_putchar(i + '0');
+			i++;
 		}
+		_putchar(i + '0');
 		i++;
 	}
 	_putchar('\n');

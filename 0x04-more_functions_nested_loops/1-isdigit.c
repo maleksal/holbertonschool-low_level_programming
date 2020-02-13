9e3d83ce58d56b1b211202e6bb31f079f40a3d53
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 08:45:21 +0000

    add func _isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  *  _isdigit - check for digits
+  *  ------------------
+  *
+  *  @c: takes an int
+  *
+  *  Return: 0 OR 1
+  *
+ **/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	} return (0);
+
+}

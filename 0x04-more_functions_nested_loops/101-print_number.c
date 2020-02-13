5211a02b792c08e71050a5f2062a192f857b0bbe
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 20:29:31 +0000

    add task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+  *  print_number - print digits
+  *  ------------------
+  *
+  *  @n: takes an int (digit)
+  *
+  *  Return: implicit return
+ **/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n / 10)
+	{
+		/* doing recursion*/
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
+

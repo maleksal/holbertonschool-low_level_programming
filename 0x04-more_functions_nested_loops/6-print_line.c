1181a287f86cb24d8cb56b5c2fa9979e0991b579
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 10:30:48 +0000

    add func print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+  *  print_line - print line n times
+  *  ------------------
+  *
+  *  @n: takes an int
+  *
+  *  Return: implicit return
+  *
+ **/
+
+void print_line(int n)
+{
+	int i = 0;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+

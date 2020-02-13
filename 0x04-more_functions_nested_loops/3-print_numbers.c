0b549fcd81097e5b0dd2949cb1297173955e5756
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 09:43:25 +0000

    add func print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  *  print_numbers - print numbers 0 to 9
+  *  ------------------
+  *
+  *  @void: void
+  *
+  *  Return: implicit return
+ **/
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i != 9)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+

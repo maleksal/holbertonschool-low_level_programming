37362923c78923d97fadf42632feb4973364ad16
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 17:53:37 +0000

    fix code

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,15 +10,19 @@
 void print_square(int size)
 {
 	int i;
-
+	int b = 0;
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-	for (i = 0; i < size; i++)
+	while (b < size)
 	{
-		_putchar('#');
+		for (i = 0; i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		b++;
 	}
-	_putchar('\n');
 }

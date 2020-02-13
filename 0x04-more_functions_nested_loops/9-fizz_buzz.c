f665df9fb2706daaac6a2ca1e6863711564d5f45
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 18:32:42 +0000

    add task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+  *  main - print numbers 1 - 100
+  *  ----------------------------
+  *
+  *  Return: return value of the function
+ **/
+
+
+int main()
+{
+	int i;
+
+	for (i = 1; i <= (100 + 1); i++)
+	{
+		if ( i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else if ( i % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else if ( i % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+	}
+	return (0);
+}
+

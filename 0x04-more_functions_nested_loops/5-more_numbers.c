668b656c13bbd3275a8c37e6c6d87c511e190c37
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 17:11:31 +0000

    fix code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,14 +18,11 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i <= 9)
-			{
-				_putchar(i + '0');
-			} else
+			if (i > 9)
 			{
 				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
 			}
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}

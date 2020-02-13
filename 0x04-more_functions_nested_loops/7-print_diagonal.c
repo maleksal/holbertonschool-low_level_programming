5916f9006f2dc57695c35a890457c5ee5a02a55b
Malek Salem <malek.salem.14@gmail.com>
2020-02-13 14:32:04 +0000

    update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+  * manage_spaces - print spaces
+  *
+  * @num: takes an int
+  * Return: implicit return
+  */
+
+void manage_spaces(int num)
+{
+	int b;
+
+	for (b = 0; b < num; b++)
+	{
+		_putchar(' ');
+	}
+}
+
+/**
+  * print_diagonal - print line
+  * @n: takes an int
+  * Return: implicit
+  */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 1; i < n + 1; i++)
+	{
+		manage_spaces(i);
+		_putchar('\\');
+		_putchar('\n');
+	}
+}

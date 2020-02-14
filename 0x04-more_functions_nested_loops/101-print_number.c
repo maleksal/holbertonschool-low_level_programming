bf6a99d917e6ec2be8c38b7645dc8884363d3e25
Malek Salem <malek.salem.14@gmail.com>
2020-02-14 11:24:56 +0000

    update code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,20 +11,31 @@
 
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n == 0)
+	
+	
+	if ( n == 0)
 	{
 		_putchar('0');
 	}
-	if (n / 10)
+	else if (n / 10 != 0)
 	{
-		/* doing recursion*/
 		print_number(n / 10);
+		if (n > 0)
+		{
+			_putchar((n % 10) + '0');
+		} else
+		{
+			_putchar((-n % 10) + '0');
+		}
 	}
-	_putchar((n % 10) + '0');
+	else if((n/10==0) && (n%10!=0) && (n>0))
+	{
+		_putchar((n % 10) + '0');
+	}
+	else if((n/10==0) && (n%10!=0) && (n<=0))
+	{
+		_putchar('-');
+		_putchar((-n % 10) + '0');
+	}
 }
 

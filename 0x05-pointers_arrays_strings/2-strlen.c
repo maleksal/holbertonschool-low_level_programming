f46c16788250c51026f85a7993d84c0f8becfcea
ssajroudi <1429@holbertonschool.com>
2020-02-18 08:03:20 +0100

    add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return length of str
+ * @s: char array
+ * Return: return int (length)
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != '\0/')
+	{
+		counter++;
+	}
+	return (counter);

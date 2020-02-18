612371dcffa4b2429ea1fb7dbb03e66284b2f4ad
Malek Salem <1419@holbertonschool.com>
2020-02-18 09:02:50 +0100

    update code

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * len_str - return len of string*
+ * @str: dtring type char
+ * Return: int (len str)
+ */
+
+int len_str(char *str)
+{
+	int counter = 0;
+
+	while (str[counter] != '\0')
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+
+/**
+ * print_rev - print str in reverse
+ * @s: str of type char
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = len_str(s); i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}

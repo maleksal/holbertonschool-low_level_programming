baa3b27f7cc007385be864efe6c8bb0f11c592a9
Malek Salem <malek.salem.14@gmail.com>
2020-02-19 21:27:31 +0000

    add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  * rev_string - modifies an array
+  * @s: arr
+  * Return: void
+  */
+
+void rev_string(char *s)
+{
+	int size = 0;
+	int index = 0
+	int temp;
+
+	while (size != '\0')
+	{
+		i++;
+	}
+
+	for (; i < n / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[(n - 1) - i];
+		s[(n - 1) - i] = temp;
+	}
+}

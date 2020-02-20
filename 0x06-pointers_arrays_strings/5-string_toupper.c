356b7a9e7faa546aedc024c5650b50586f49e09a
Malek Salem <malek.salem.14@gmail.com>
2020-02-20 16:33:38 +0000

    add task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * string_toupper - convert string from lower to upper
+  * Return: char converted
+  */
+
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	for (; str[index] != '\0'; index++)
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			str[index] = str[index] - 32;
+	
+		}
+	}
+	return (str);
+}

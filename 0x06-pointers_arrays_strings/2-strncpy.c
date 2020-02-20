21b3f679528bdbe06843435b8383841fb4778adb
Malek Salem <malek.salem.14@gmail.com>
2020-02-20 14:46:39 +0000

    fix code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,6 +16,9 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[index] = src[index];
 	}
-	dest[index] = '\0';
+	for (; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
 	return (dest);
 }

8eb3d16ec6b5877c76548a8b5181e89a107c139d
Malek Salem <malek.salem.14@gmail.com>
2020-02-20 15:08:54 +0000

    add task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  * _strcmp - compare two strings
+  * @s1: type int
+  * @s2: type int
+  * Return: 0 if string match, less than 0 or greater if they dont match
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0')
+	{
+		if (s1[i] > s2[i])
+		{
+			return (15);
+		} else if (s1[i] < s2[i])
+		{
+			return (-15);
+		}
+		i++;
+	}
+	return (0);
+}

1ae8fea3fcb7f6105bddfb20139bffaeb5655d99
Malek Salem <malek.salem.14@gmail.com>
2020-02-20 12:55:43 +0000

    add task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * _strncpy - copies a string
+  * @dest: type char
+  * @src: type char
+  * @n: type int
+  * Return: pointer to dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; (index < n) && (src[index] != '\0'); index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = '\0';
+	return (dest);
+}

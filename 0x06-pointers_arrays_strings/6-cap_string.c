e198b0a41fea52a9151ce4d1c5c89506ff3d4898
Malek Salem <malek.salem.14@gmail.com>
2020-02-20 20:37:24 +0000

    fix betty warning

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,13 @@ char *cap_string(char *str)
 {
 	int i = 0;
 	int ii;
-	int sep[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
+	int s[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
 
 	while (str[i] != '\0')
 	{
-		for (ii = 0; sep[ii] != '\0'; ii++)
+		for (ii = 0; s[ii] != '\0'; ii++)
 		{
-			if (str[i] == sep[ii])
+			if (str[i] == s[ii])
 			{
 				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				{

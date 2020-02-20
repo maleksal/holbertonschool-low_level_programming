110f28909cc662a9c3d4ca52e6a915ce56e4a4e3
Malek Salem <malek.salem.14@gmail.com>
2020-02-20 16:10:26 +0000

    update code logic

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,12 +13,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0')
 	{
-		if (s1[i] > s2[i])
+		if (s1[i] !=  s2[i])
 		{
-			return (15);
-		} else if (s1[i] < s2[i])
-		{
-			return (-15);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}

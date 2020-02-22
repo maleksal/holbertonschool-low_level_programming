d60163f3d26e4f5346ae02c830204734028bbe76
Malek Salem <malek.salem.14@gmail.com>
2020-02-22 16:29:14 +0000

    update code

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+  * rot13 - encrypt char
+  * @c: char type
+  * Return: encrypted c(arr)
+  */
+
+char *rot13(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		for ( ; (c[i] >= 'A' && c[i] <= 'Z') || (c[i] >= 'a' && c[i] <= 'z'); )
+		{
+			if ((c[i] >= 'n') || (c[i] > 'N'))
+			{
+			c[i] = *(c + i) - 13;
+			break;
+			}
+			c[i] = *(c + i) + 13;
+			break;
+		}
+		i++;
+	}
+	return (c);
+}

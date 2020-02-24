5de38df88ad5a637b6c063f7813ddd7c31a48e7c
dorradalyy <dorradaly01@gmail.com>
2020-02-24 20:34:43 +0100

    add task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: type char
+ * @c: type of char
+ * Return: pointer to char or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s[i]);
+		}
+		return;
+	}
+}

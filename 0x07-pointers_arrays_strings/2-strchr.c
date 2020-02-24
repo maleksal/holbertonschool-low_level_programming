df37e55c4415c4b7631d28fc6e00d7d69a57dfe1
dorradalyy <dorradaly01@gmail.com>
2020-02-24 21:04:08 +0100

    fix code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,11 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for ( ; *s; s++)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (&s[i]);
+			return (s);
 		}
 	}
 	return (NULL);

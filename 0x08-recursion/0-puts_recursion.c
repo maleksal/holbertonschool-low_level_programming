3352b1e4a39323f69e88aef97fa215a8a074812c
malek salem <malek.salem.14@gmail.com>
2020-02-26 14:13:19 +0100

    add task 0*

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_recursion - print str
+ * @s: type char
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}

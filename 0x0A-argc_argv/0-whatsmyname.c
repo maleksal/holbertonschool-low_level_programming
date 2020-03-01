a9a78f98e5c928c5ab881fffdbb928574f05d0a3
Malek Salem <malek.salem.14@gmail.com>
2020-03-01 13:12:43 +0000

    fix betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
 /**
   * main - print program name
   * @argc: size of array argv
-  * @argv: array 
+  * @argv: array
   * Return: int
   */
 
 int main(int argc, char **argv)
 {
 	int i;
-	
+
 	(void)argc;
 	for (i = 0; argv[0][i] != '\0'; i++)
 	{

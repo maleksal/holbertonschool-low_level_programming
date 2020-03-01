f5f303c7abef3335aa509bd1ed9100772e4240b6
Malek Salem <malek.salem.14@gmail.com>
2020-03-01 13:20:48 +0000

    add task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * main - print number of argvs
+  * @argc: size of arr argv
+  * @argv: arr
+  * Return: int
+  */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	_putchar(argc + '0');
+	_putchar('\n');
+	return (0);
+}

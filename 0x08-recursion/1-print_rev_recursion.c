21fd747950ebc53ce95122e6428f7020d7c14ed3
Malek Salem <malek.salem.14@gmail.com>
2020-03-02 21:52:43 +0000

    add task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * _print_rev_recursion - a non-tail recursive function 
+  * that prints an array in reverse
+  * -----------------------------------------------------
+  * @s: arr
+  * Return: void
+  */
+
+void _print_rev_recursion(char *s)
+{
+	/* base condition */
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}

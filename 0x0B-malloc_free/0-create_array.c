2ab70af35172a12ac046546513c46cf5473a6e6b
Malek Salem <malek.salem.14@gmail.com>
2020-03-03 14:37:04 +0000

    add task 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * create_array - create array and intialized with a char
+  * @size: unsigned int
+  * @char: char type
+  * return: char
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array_pointer = malloc(sizeof(*array_pointer) * size);
+	unsigned int i = 0;
+
+	if (array_pointer == '\0')
+		return (NULL);
+	else if (size == 0)
+		return ('\0');
+
+	for (; i <= size; i++)
+	{
+		array_pointer[i] = c;
+	}
+	return (array_pointer);
+
+}

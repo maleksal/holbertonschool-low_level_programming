37cc8d14bec8030d4cd977622b6b2bc041885d9a
Malek Salem <malek.salem.14@gmail.com>
2020-03-06 09:10:57 +0000

    update code

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * strdup - allocate space in memory
+  * @str: string
+  * Return: void
+  */
+
+char *_strdup(char *str)
+{
+	/* initialize */
+	unsigned int size_arr = 0;
+	char *p;
+	unsigned int i;
+
+	/* calculate array size */
+	for (; str[size_arr] != '\0'; size_arr++);
+	
+	/* locate space in memory */
+	p = malloc(sizeof(char) * size_arr + 1);
+	
+	if (p == NULL || str == NULL)
+		return (NULL);
+	
+	for (i = 0; i <= size_arr; i++)
+	{
+		p[i] = *str;
+		str++;
+	}
+	return (p);
+}

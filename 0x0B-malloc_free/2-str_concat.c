bf9bb407fe7694180ee7b1e5368a4d93e017ca8e
Malek Salem <malek.salem.14@gmail.com>
2020-03-06 10:43:04 +0000

    add task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * str_concat - concat two strings
+  * @s1: str
+  * @s2: str
+  * Return: char type
+  */
+
+
+char *str_concat(char *s1, char *s2)
+{
+	int size_2_arrays = 1;
+	char *p;
+	char *addr_s1 = s1;
+	char *addr_s2 = s2;
+	int i = 0;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		s1 = '\0';
+		s2 = '\0';
+	}
+	/* size s1 */
+	for (; *s1 != '\0'; s1++)
+		size_2_arrays++;
+	/* size s2 */
+	for (; *s2 != '\0'; s2++)
+		size_2_arrays++;
+	/* relocate pointer */
+	s1 = addr_s1;
+	s2 = addr_s2;
+	/* locate space in memory */
+	p = malloc(sizeof(char *) * size_2_arrays);
+	if (p == NULL)
+		return (NULL);
+	for ( ; *s1 != '\0'; s1++)
+	{
+		p[i] = *s1;
+		i++;
+	}
+	for ( ; *s2 != '\0'; s2++)
+	{
+		p[i] = *s2;
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}

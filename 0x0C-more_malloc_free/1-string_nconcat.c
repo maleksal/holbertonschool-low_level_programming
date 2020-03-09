4f0b8670b57d6bdc3ba4ec375f2da35ad890fed8
Malek Salem <malek.salem.14@gmail.com>
2020-03-09 19:17:56 +0000

    add task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+
+
+
+/**
+  * len - get length of string
+  * @s: str
+  * Return : int
+  */
+
+int len(char *s)
+{
+	int c;
+	for (c = 0; s[c] != '\0'; c++)
+	;
+	return (c);
+}
+
+/**
+  * string_nconcat - concatenates two strings
+  * @s1: string
+  * @s2: string
+  * @n: unsigned int
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *array;
+	unsigned int i = 0;
+	unsigned int i_s2;
+	unsigned size_s2 = len(s2);
+	unsigned size = len(s1) + len(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	array = malloc(sizeof(char) * (size + 1));
+	
+	/* check for allocation failure */
+	if (array == NULL)
+		return (NULL);
+	
+	for ( ; *s1 != '\0'; s1++)
+	{
+		array[i] = *s1;
+		i++;
+	}
+	if (n >= size_s2)
+	{
+		for ( ; *s2 != '\0'; s2++)
+		{
+			array[i] = *s2;
+			i++;
+		}
+	}
+	for (i_s2 = 0; i_s2 <= n; i_s2++)
+	{
+		array[i] = *s2;
+		s2++;
+		i++;
+	}
+	array[i] = '\0';
+	return (array);
+}

f6406848365a0eb148d19f601d3b0d9cf16dd66f
Malek Salem <malek.salem.14@gmail.com>
2020-03-09 22:48:19 +0000

    update code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef STRUCT_FILE
 #define STRUCT_FILE
 
+
+/**
+  * struct dog - contains 3 elements
+  * @name: pointer to array
+  * @age: float
+  * @owner: pointer to array
+  */
+
 struct dog
 {
 	char *name;

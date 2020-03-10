2c835adfad5691b9963bfe5036fdd217eabd8753
Malek Salem <malek.salem.14@gmail.com>
2020-03-10 13:42:57 +0000

    update code

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
   * init_dog - initialize struct
   * @d: pointer to struct
@@ -9,8 +10,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == '\0')
-		return ('\0');
+	if (d == NULL)
+		return (NULL);
 
 	d->name = name;
 	d->age = age;

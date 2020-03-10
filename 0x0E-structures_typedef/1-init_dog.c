fce416fe916027a8ada83c5248f1efc082b8ef05
Malek Salem <malek.salem.14@gmail.com>
2020-03-10 13:39:17 +0000

    update ceod

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+
 /**
   * init_dog - initialize struct
   * @d: pointer to struct
@@ -15,4 +16,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	
+	return (d);
 }

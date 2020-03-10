019a194ca38681b9c10a81389da125704938a2ee
Malek Salem <malek.salem.14@gmail.com>
2020-03-10 20:56:30 +0000

    add task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include "dog.h"
+#include <stdlib.h>
+
+#define C
+#define CHECK(p) (p == NULL)
+#define LEN(s) (sizeof(s)/sizeof(s[0]))
+
+/**
+  * new_dog - create new object for struct
+  * @name: array char
+  * @age: float
+  * @owner: array char
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	unsigned int c;
+	dog_t *dog_obj = malloc(sizeof(dog_t));
+
+	/* check for allocation failure */
+	if (CHECK(dog_obj))
+		return (NULL);
+
+	/* locate space for name */
+	dog_obj->name = malloc(LEN(name) + 1);
+	
+	/* check for failure */
+	if (CHECK(dog_obj->name))
+	{
+		free(dog_obj);
+		return (NULL);
+	}
+
+	dog_obj->owner = malloc(LEN(owner) + 1);
+	
+	/* check for failure */
+	if (CHECK(dog_obj->owner))
+	{
+		free(dog_obj->name);
+		free(dog_obj);
+		return (NULL);
+	}
+	dog_obj->age = age;
+	
+	/* assign values */
+	for (c = 0; c < LEN(name); c++)
+		dog_obj->name[c] = name[c];
+	for (c = 0; c < LEN(owner); c++)
+		dog_obj->owner[c] = owner[c];
+
+	/* return pointer */
+	return (dog_obj);
+}

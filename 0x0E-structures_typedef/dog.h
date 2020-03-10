78e977e7a0d65b56793e6d4217ed01c7a4b92f99
Malek Salem <malek.salem.14@gmail.com>
2020-03-10 13:45:31 +0000

    fix code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
   * @owner: pointer to array
   */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} ;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif

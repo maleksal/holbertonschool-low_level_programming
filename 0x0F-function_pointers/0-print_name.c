7e4f6f4d563306633f88b3b50d34756c3949b9a3
Malek Salem <malek.salem.14@gmail.com>
2020-03-12 22:43:18 +0000

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+  * print_name - print name using function pointer
+  * @name: string
+  * @f: pointer to function
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+# learn function pointers

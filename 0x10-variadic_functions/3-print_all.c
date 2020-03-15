a34e085536111f504bc9a80670b434dc18dd9a03
Malek Salem <malek.salem.14@gmail.com>
2020-03-15 12:18:24 +0000

    add task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,95 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+
+
+/**
+  * _c - print character
+  * @c: variable
+  */
+
+void _c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+  * _i - print digit
+  * @i: variable
+  */
+
+void _i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+  * _f - print float
+  * @f: variable
+  */
+
+void _f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+  * _s - print string
+  * @s: variable
+  */
+
+void _s(va_list s)
+{
+	char *Ptr = va_arg(s, char *);
+
+	if (Ptr != NULL)
+	{
+		printf("%s", Ptr);
+	}
+	else
+		printf("(nil)");
+}
+
+
+
+
+void print_all(const char * const format, ...)
+{
+
+	unsigned int i = 0, b;
+	/* pointer to function */
+	void (*fp)(va_list);
+	char *separator = "";
+	arrayF array[] = {
+		{"c",  _c},
+		{"i",  _i},
+		{"f",  _f},
+		{"s", _s},
+		{NULL, NULL}
+	};
+
+	va_list formatPtr;
+
+	va_start(formatPtr, format);
+	
+	while (format && format[i])
+	{
+		b = 0;
+		while (array[b].ch != NULL)
+		{
+			if (array[b].ch[0] == format[i])
+			{
+				printf("%s", separator);
+				fp = array[b].fp;
+				fp(formatPtr);
+				separator = ", ";
+			}
+			b++;
+		}
+		i++;
+	}
+	
+	va_end(formatPtr);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,24 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
+typedef struct format
+{
+	char *ch;
+	void (*fp)(va_list);
+} arrayF;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+void _c(va_list c);
+void _i(va_list i);
+void _f(va_list f);
+void _s(va_list s);
+
 #endif

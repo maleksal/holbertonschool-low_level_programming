8d435641c9fe52cf7531f71d6697897fc5131a92
Malek Salem <malek.salem.14@gmail.com>
2020-03-21 22:14:19 +0000

    add task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+  * print_list - print elements of a linked list
+  * @h: singly linked list
+  * Return: number of nodes
+  */
+
+size_t print_list(const list_t *h)
+{
+	int nodes = 0;
+	const list_t *ptr;
+	
+	ptr = h;
+
+	/* acess list */
+	
+	while (ptr != NULL)
+	{
+		/* check if string is null */
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			/* print elem */
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		
+		
+		nodes++; /* count node */
+		ptr = ptr->next; /* move to next node */
+	}
+	return (nodes); /* return number of nodes */
+
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stddef.h>
+
+/**
+  * struct Node - user defined data
+  * @str: type of char
+  * @len: type of int
+  * @next: object refernece pointer
+  * Description: Node
+  */
+
+typedef struct Node
+{
+	char *str;
+	int len;
+	struct Node *next;
+} list_t;
+
+
+size_t print_list(const list_t *h);
+
+#endif

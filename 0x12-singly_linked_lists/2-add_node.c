ea303ef91a5fd44934e08e12b2394b69cc2f0147
Malek Salem <malek.salem.14@gmail.com>
2020-03-21 23:52:50 +0000

    add _strlen function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+/**
+  * _strlen - calculate length of sting
+  * @s: string
+  * Return: int
+  */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	;
+	return (i);
+}
+
 /**
   * add_node - insert node at the beginning of list
   * @head: pointer
@@ -14,23 +30,29 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
+	int strlen = _strlen(str);
 
 	/* locate memory for temp */
 	temp = (list_t *) malloc(sizeof(list_t));
 
 	/* check if string is null */
 	if (!str)
+	{
 		temp->str = NULL;
-	else
+		temp->len = 0;
+	} else
 	{
 		/* allocate memory for temp->str */
-		temp->str = malloc((sizeof(str) / sizeof(str[0])) + 1);
+		temp->str = malloc(strlen + 1);
 
 		/* check for failure */
 		if (!temp->str)
 			return (NULL);
 		/* duplicate str */
 		temp->str = strdup(str);
+		/* add string length in temp->len */
+		temp->len = strlen;
+
 	}
 	/* check if first elem */
 	if (!head)

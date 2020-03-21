4fc9e0f630f7d0fd7c00001dbc6b3cc03f5fb5a5
Malek Salem <malek.salem.14@gmail.com>
2020-03-21 22:24:27 +0000

    add task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * list-len - countes number of elements in a singly linked list
+  * @h: pointer
+  * Return: type of int
+  */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *pointer = h;
+	int len = 0;
+
+	while (pointer)
+	{
+		len++;
+		h = h->next;
+		pointer = h;
+	}
+	return (len);
+
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,5 +20,5 @@ typedef struct Node
 
 
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
 #endif

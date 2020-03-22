bd2b3ac79ab3bcd2aa3fe931ecd147ee2148a065
Malek Salem <malek.salem.14@gmail.com>
2020-03-22 01:33:53 +0000

    add advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_before_execution(void) __attribute__ ((constructor));
+
+/**
+  * print_before_execution - print before main
+  */
+
+void print_before_execution(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

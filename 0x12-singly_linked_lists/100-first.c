04f7f7ff7dd2e355db1b0c7f1e0e7caef14c9dba
Malek Salem <malek.salem.14@gmail.com>
2020-03-22 01:35:43 +0000

    fix betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,6 @@ void print_before_execution(void) __attribute__ ((constructor));
 
 void print_before_execution(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

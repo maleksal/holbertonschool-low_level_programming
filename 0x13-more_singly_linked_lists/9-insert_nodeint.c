2705acd5c886eadc681f2fbb94add4ab6e58a4f5
Malek Salem <malek.salem.14@gmail.com>
2020-03-24 17:15:51 +0000

    fix logic

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node->n = n;
 	node->next = NULL;
 
-	if (*head == NULL)
+	if (*head == NULL || idx == 0)
 	{
 		if (idx == 0)
 		{	node->next = *head;

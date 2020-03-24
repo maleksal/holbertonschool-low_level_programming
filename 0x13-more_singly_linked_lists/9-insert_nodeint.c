8b10d8f5b6e20e467db988b8ca6af4723fa85fc0
Malek Salem <malek.salem.14@gmail.com>
2020-03-24 16:40:54 +0000

    add task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+
+/**
+  * insert_nodeint_at_index - insert node at a given position
+  * @head: pointer
+  * @idx: unsigned int
+  * @n: int
+  * Return: adress
+  */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int index = 0;
+	listint_t *node;
+	listint_t *ptr;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->next = NULL;
+
+	ptr = *head;
+	while (ptr->next != NULL)
+	{
+		if (idx == 0)
+		{
+			if (*head != NULL)
+				node->next = *head;
+			*head = node;
+			return (node);
+		}
+
+		else if (index == (idx - 1))
+		{
+			node->next = ptr->next;
+			ptr->next = node;
+			return (node);
+		}
+		ptr = ptr->next;
+		index++;
+	}
+	free(node);
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,6 +25,7 @@ typedef struct Node
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int pop_listint(listint_t **head);
 int sum_listint(listint_t *head);
 void free_listint(listint_t *head);

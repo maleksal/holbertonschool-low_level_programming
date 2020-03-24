f9f4655949ef0f2d6417f405f9ccbff0de3a9ed1
Malek Salem <malek.salem.14@gmail.com>
2020-03-24 14:42:28 +0000

    fix bug

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		ptr = *head;
 		*head = ptr->next;
 		free(ptr);
 	}
-	head = NULL;
+
+	*head = NULL;
 }

3a5d4e890eb50a7c23001448e19c8900f772b865
Malek Salem <malek.salem.14@gmail.com>
2020-03-24 19:22:29 +0000

    add task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+  * delete_nodeint_at_index - delete node at nth position
+  * @head: pointer
+  * @index: unsigned int
+  * Return: int
+  */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+	unsigned int i = 0;
+	listint_t *ptr;
+	listint_t *node;
+
+	if (!head || *head == NULL)
+		return (-1);
+
+	ptr = *head;
+	if (index == 0)
+	{
+		*head = ptr->next;
+		free(ptr);
+		return (1);
+	}
+
+	while (ptr->next != NULL)
+	{
+		if (i == (index - 1))
+		{
+			node = ptr->next;
+			ptr = node->next;
+			free(node);
+			return (1);
+		}
+		ptr = ptr->next;
+		i++;
+	}
+	return (-1);
+}

6ef713bee2de75e406cb1e58bcf561dd9ae9db85
Malek Salem <malek.salem.14@gmail.com>
2020-03-24 19:26:21 +0000

    fix bug

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (i == (index - 1))
 		{
 			node = ptr->next;
-			ptr = node->next;
+			ptr->next = node->next;
 			free(node);
 			return (1);
 		}

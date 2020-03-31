435f8034dbc8e10e0d86613a77a9e0bf1d4364a9
Malek Salem <malek.salem.14@gmail.com>
2020-03-31 19:35:50 +0000

    fix betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,9 +2,10 @@
 
 
 /**
-  *
-  *
-  *
+  * append_text_to_file - append text to a file
+  * @filename: pointer
+  * @text_content: string
+  * Return: 1 if success OR -1 if fail
   */
 
 int append_text_to_file(const char *filename, char *text_content)
@@ -14,14 +15,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	
+
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd < 0)
 		return (-1);
 
 	if (!text_content)
 		return (1);
-	
+
 	for (i = 0; text_content[i]; i++)
 	;
 	wr = write(fd, text_content, i);

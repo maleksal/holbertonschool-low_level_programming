9848c7584611486921ba2288563a8f2d503bbef0
Malek Salem <malek.salem.14@gmail.com>
2020-03-31 17:20:28 +0000

    add task 0 && README.md

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+  *
+  *
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, wr, re;
+	char * buffer;
+
+	/* check for null */
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(letters));
+	if (!buffer)
+		return (0);
+	
+	/* open file & check for failure */
+	fd = open(filename, O_RDONLY);
+	CHECK_CALL(fd);
+
+	re = read(fd, buffer, letters);
+	CHECK_CALL(re);
+
+	wr = write(STDOUT_FILENO, buffer, re);
+	CHECK_CALL(wr);
+
+	return (wr);
+
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,20 @@
+#ifndef H
+#define H
+
+
+#define CHECK_CALL(F) { if (F <= 0) return (0);}
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+#endif

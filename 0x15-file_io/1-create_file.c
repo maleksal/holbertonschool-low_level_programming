7f0e93d9508519a486c6cf509bb18ea7062de1f8
Malek Salem <malek.salem.14@gmail.com>
2020-03-31 19:08:57 +0000

    add task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+
+/**
+  * create_file - create file from with a given name and content
+  * @filename: pointer
+  * @text_content: string
+  * Return: number of written bytes
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd < 0)
+		return (0);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+		;
+		wr = write(fd, text_content, i);
+		if (wr < 0)
+			return (-1);
+
+		return (wr);
+	}
+
+	return (0);
+}

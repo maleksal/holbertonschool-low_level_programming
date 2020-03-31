97220ceee580caf3fdef38e060490ef7fe3346de
Malek Salem <malek.salem.14@gmail.com>
2020-03-31 19:19:21 +0000

    fix bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd < 0)
 		return (-1);
 

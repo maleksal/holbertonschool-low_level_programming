c6b05fb41eb1781a92e0cc2eb271d23c524f5689
Malek Salem <malek.salem.14@gmail.com>
2020-05-01 17:28:52 +0000

    add task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a doubly linked list
+ * @head: pointer to doubly linked list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+
+	while (head)
+	{
+		if (head->prev != NULL)
+		{
+			free(head->prev);
+		}
+
+		if (head->next == NULL)
+		{
+			free(head);
+			exit(0);
+		}
+
+		head = head->next;
+	}
+
+}

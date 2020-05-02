e398ebcc6286eff675c78e88b08e0e23efea6095
Malek Salem <malek.salem.14@gmail.com>
2020-05-02 20:41:03 +0000

    update this mo**erfu**er

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,28 +24,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (ptr)
 	{
-		if (pos + 1 == idx && ptr->next != NULL)
+		if (pos == idx && ptr->next != NULL)
 		{
-			if (ptr->next->next == NULL)
-			{
-				new_node = add_dnodeint_end(h, n);
-				return (new_node);
-			}
-
 			new_node = malloc(sizeof(dlistint_t));
 			if (!new_node)
 				return (NULL);
 
 			new_node->n = n;
-			new_node->next = ptr->next;
-			new_node->prev = ptr;
-			ptr->next->prev = new_node;
-			ptr->next = new_node;
+			new_node->next = ptr;
+			new_node->prev = ptr->prev;
+			ptr->prev->next = new_node;
+			ptr->prev = new_node;
 
 			return (new_node);
 		}
 		ptr = ptr->next;
 		pos++;
+	}
+
+	if (idx == pos - 1)
+	{
+		new_node = add_dnodeint_end(h, n);
+		return (new_node);
 	}
 	return (NULL);
 

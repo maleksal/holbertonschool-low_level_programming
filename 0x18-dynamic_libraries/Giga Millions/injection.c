917673ae6e0334b16b78fd0a4225c2c64a5f4a76
Malek Salem <malek.salem.14@gmail.com>
2020-05-05 00:09:33 +0000

    hack si checker

--- 0x18-dynamic_libraries/Giga Millions/injection.c
+++ 0x18-dynamic_libraries/Giga Millions/injection.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int puts(const char *s){
-	(void) s;
+int atoi(const char *nptr){
+	(void) nptr;
 
 	write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 57);
 	exit(0);

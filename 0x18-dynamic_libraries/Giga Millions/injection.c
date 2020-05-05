431a54d745a04b24ca9f7fa80136e7f285cadbb8
Malek Salem <malek.salem.14@gmail.com>
2020-05-05 00:17:41 +0000

    set len message 55

--- 0x18-dynamic_libraries/Giga Millions/injection.c
+++ 0x18-dynamic_libraries/Giga Millions/injection.c
@@ -4,6 +4,6 @@
 int atoi(const char *nptr){
 	(void) nptr;
 
-	write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 57);
+	write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 55);
 	exit(0);
 }

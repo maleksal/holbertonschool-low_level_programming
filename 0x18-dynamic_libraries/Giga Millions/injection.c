92f757d71478cc2ad47fc48c6beb76c65649a491
Malek Salem <malek.salem.14@gmail.com>
2020-05-05 00:06:27 +0000

    update what si chekcer wants

--- 0x18-dynamic_libraries/Giga Millions/injection.c
+++ 0x18-dynamic_libraries/Giga Millions/injection.c
@@ -4,6 +4,6 @@
 int puts(const char *s){
 	(void) s;
 
-	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 57);
 	exit(0);
 }

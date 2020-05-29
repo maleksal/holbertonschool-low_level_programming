1f2873366b9972c178f5737fb0b5836c7dd422f3
Malek Salem <malek.salem.14@gmail.com>
2020-05-29 22:07:34 +0000

    add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash table in key value order
+ * @ht: pointer to hashtable
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *pointerNode;
+	unsigned int idx;
+	int checker = 0;
+
+
+	if (!ht)
+		return;
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		pointerNode = ht->array[idx];
+		while (pointerNode)
+		{
+			if (checker)
+				printf(", ");
+			printf("'%s':'%s'", pointerNode->key, pointerNode->value);
+			pointerNode = pointerNode->next;
+			checker++;
+		}
+	}
+	printf("}\n");
+}

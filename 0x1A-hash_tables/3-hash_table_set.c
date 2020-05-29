dfe33ff26d3a05f1a97b13f868383f64b7f72d60
Malek Salem <malek.salem.14@gmail.com>
2020-05-29 15:47:20 +0000

    check for NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *pointer;
 	unsigned long int tableIndex;
 
-	if (!key || !*key || !ht)
+	if (!key || !*key || !value || !ht)
 		return (0);
 
 	/* create node */

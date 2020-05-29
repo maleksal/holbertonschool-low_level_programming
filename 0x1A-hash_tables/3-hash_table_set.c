f238453127f70bebf1dd6ce3b6e0ac1e3851edc6
Malek Salem <malek.salem.14@gmail.com>
2020-05-29 15:37:42 +0000

    fix betty style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - insert item in a key value hash table
+ * @ht: hash table
+ * @key: const char *
+ * @value: const char *
+ * Return: 0 if success, 1 if fail
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -10,8 +17,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!key || !ht)
 		return (1);
 
-	
-	
 	/* create node */
 	newElement = malloc(sizeof(hash_node_t));
 	if (!newElement)
@@ -19,14 +24,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	newElement->key = (char *)key;
 	newElement->value = strdup(value);
-	
+
 	/* hash key and get index */
 	tableIndex = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[tableIndex] != NULL)
 	{
 		pointer = ht->array[tableIndex];
-		
+
 		/* add node to start of linked list */
 		newElement->next = pointer->next;
 		pointer->next = newElement;

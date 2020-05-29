ec9965ad5c78d327debc5f0f2f1889f5ec4f9ddf
Malek Salem <malek.salem.14@gmail.com>
2020-05-29 15:58:33 +0000

    fix collision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,17 +11,15 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newElement;
-	hash_node_t *pointer;
+	hash_node_t *pointer, *walk;
 	unsigned long int tableIndex;
 
 	if (!key || !*key || !value || !ht)
 		return (0);
-
 	/* create node */
 	newElement = malloc(sizeof(hash_node_t));
 	if (!newElement)
 		return (0);
-
 	newElement->key = (char *)key;
 	newElement->value = strdup(value);
 	if (newElement->value == NULL)
@@ -31,11 +29,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	/* hash key and get index */
 	tableIndex = key_index((const unsigned char *)key, ht->size);
-
 	if (ht->array[tableIndex] != NULL)
 	{
-		pointer = ht->array[tableIndex];
-
+		walk = ht->array[tableIndex];
+		pointer = walk;
+		while (walk)
+		{
+			if (strcmp(walk->key, key) == 0)
+			{
+				free(walk);
+				break;
+			}
+			walk = walk->next;
+		}
 		/* add node to start of linked list */
 		newElement->next = pointer->next;
 		pointer->next = newElement;

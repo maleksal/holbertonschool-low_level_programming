f09a210fbaee2d20181869c2320fc73759f55dff
Malek Salem <malek.salem.14@gmail.com>
2020-05-29 21:30:47 +0000

    fix code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 
+
 /**
  * hash_table_set - insert item in a key value hash table
  * @ht: hash table
@@ -10,13 +11,26 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newElement;
-	hash_node_t *pointer, *walk;
+	hash_node_t *newElement, *walk;
 	unsigned long int tableIndex;
 
 	if (!key || !*key || !value || !ht)
 		return (0);
-	/* create node */
+	tableIndex = key_index((const unsigned char *) key, ht->size);
+	walk = ht->array[tableIndex];
+	while (walk)
+	{
+		if (strcmp(key, walk->key) == 0)
+		{
+			free(walk->value);
+			walk->value = strdup(value);
+			if (!walk->value)
+				return (0);
+			return (1);
+		}
+		walk = walk->next;
+	}
+
 	newElement = malloc(sizeof(hash_node_t));
 	if (!newElement)
 		return (0);
@@ -27,25 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(newElement);
 		return (0);
 	}
-	/* hash key and get index */
-	tableIndex = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[tableIndex] != NULL)
-	{
-		walk = ht->array[tableIndex];
-		pointer = walk;
-		while (walk)
-		{
-			if (strcmp(walk->key, key) == 0)
-			{
-				free(walk);
-				break;
-			}
-			walk = walk->next;
-		}
-		/* add node to start of linked list */
-		newElement->next = pointer->next;
-		pointer->next = newElement;
-	}
+	newElement->next = ht->array[tableIndex];
 	ht->array[tableIndex] = newElement;
 	return (1);
 }

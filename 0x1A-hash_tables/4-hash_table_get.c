cd163d4bf8bf2d54881aa416452869408da47d9e
Malek Salem <malek.salem.14@gmail.com>
2020-05-29 21:43:07 +0000

    check for NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int indexKey;
 	hash_node_t *pointer;
 
+	if (!ht || key)
+		return (NULL);
 	/* get index */
 	indexKey = key_index((const unsigned char *)key, ht->size);
 

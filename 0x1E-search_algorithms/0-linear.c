8265cf0087a421baeba894b54866ca31b29e0bd6
Malek Salem <malek.salem.14@gmail.com>
2020-08-28 11:47:06 +0000

    add task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - function that searches for a value in an
+  * array of integers using the Linear search algorithm.
+  * @array: array of elements
+  * @size: size of array
+  * @value: value to be searched for
+  * Return: value or -1 on fail
+  */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int idx;
+
+	if (array)
+	{
+
+		for (idx = 0; idx < size; idx++)
+		{
+			if (array[idx] == value)
+				return (value);
+			printf("Value checked array[%d] = [%i]\n", idx, array[idx]);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef __HEADER__
+#define __HEADER__
+
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+
+
+#endif
